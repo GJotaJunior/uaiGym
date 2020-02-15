889d481bf9ce5c43dfc1f121ed65ebad18e1f57c
Nawan <nawandx743@gmail.com>
2020-02-15 02:12:35 -0300

    Criando classe pessoa dto

---
+++ ifit/src/main/java/ifit/model/dto/Endereco.java
@@ -0,0 +1,26 @@
+package ifit.model.dto;
+
+public class Endereco {
+	String rua;
+	Integer numero;
+	String bairro;
+	
+	public String getRua() {
+		return rua;
+	}
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+	public Integer getNumero() {
+		return numero;
+	}
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+}
---
+++ ifit/src/main/java/ifit/model/enuns/EnumSexo.java
@@ -0,0 +1,5 @@
+package ifit.model.enuns;
+
+public enum EnumSexo {
+	MASCULINO, FEMININO;
+}
