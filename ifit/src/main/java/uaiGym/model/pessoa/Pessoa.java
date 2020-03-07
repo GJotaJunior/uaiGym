cf2d0770f608f9425cec4ede28d3278671ec3174
Wanderson Gutemberg <wanderson.gutembergs@gmail.com>
2020-03-07 14:28:47 -0300

    Alterando atributo telefone para lista e retirando alguns sets

--- ifit/src/main/java/uaiGym/model/pessoa/Pessoa.java
+++ ifit/src/main/java/uaiGym/model/pessoa/Pessoa.java
@@ -10,78 +10,79 @@ import uaiGym.model.enuns.PerfilEnum;
 import uaiGym.model.enuns.SexoEnum;
 
 public abstract class Pessoa {
-	
-	private String nome;
-	private String cpf;
-	private Date nascimento;
-	private String telefone;
-	private SexoEnum sexo;
-	private Endereco endereco;
-	private PerfilEnum perfil;
-	private List<ContatoDeEmergencia> contatosDeEmergencia;
-	
-	public Pessoa(String nome, String cpf, Date nascimento, String telefone, SexoEnum sexo, Endereco endereco, PerfilEnum perfil) {
-		this.nome = nome;
-		this.cpf = cpf;
-		this.nascimento = nascimento;
-		this.telefone = telefone;
-		this.sexo = sexo;
-		this.endereco = endereco;
-		this.perfil = perfil;
-		this.contatosDeEmergencia = new ArrayList<ContatoDeEmergencia>();
-	}
-	
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public String getCpf() {
-		return cpf;
-	}
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-	public Date getNascimento() {
-		return nascimento;
-	}
-	public void setNascimento(Date nascimento) {
-		this.nascimento = nascimento;
-	}
-	public String getTelefone() {
-		return telefone;
-	}
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
-	}
-	public SexoEnum getSexo() {
-		return sexo;
-	}
-	public void setSexo(SexoEnum sexo) {
-		this.sexo = sexo;
-	}
-	public Endereco getEndereco() {
-		return endereco;
-	}
-	public void setEndereco(Endereco endereco) {
-		this.endereco = endereco;
-	}
 
-	public PerfilEnum getPerfil() {
-		return perfil;
-	}
+    private String nome;
+    private String cpf;
+    private Date nascimento;
+    private List<String> telefone;
+    private SexoEnum sexo;
+    private Endereco endereco;
+    private PerfilEnum perfil;
+    private List<ContatoDeEmergencia> contatosDeEmergencia;
 
-	public void setPerfil(PerfilEnum perfil) {
-		this.perfil = perfil;
-	}
+    public Pessoa(String nome, String cpf, Date nascimento, String telefone, SexoEnum sexo, Endereco endereco,
+	    PerfilEnum perfil) {
+	this.nome = nome;
+	this.cpf = cpf;
+	this.nascimento = nascimento;
+	this.telefone = new ArrayList<String>();
+	this.telefone.add(telefone);
+	this.sexo = sexo;
+	this.endereco = endereco;
+	this.perfil = perfil;
+	this.contatosDeEmergencia = new ArrayList<ContatoDeEmergencia>();
+    }
 
-	public List<ContatoDeEmergencia> getContatosDeEmergencia() {
-		return contatosDeEmergencia;
-	}
+    public String getNome() {
+	return nome;
+    }
 
-	public void addContatoDeEmergencia(ContatoDeEmergencia contatoDeEmergencia) {
-		contatosDeEmergencia.add(contatoDeEmergencia);
-	}
-	
+    public void setNome(String nome) {
+	this.nome = nome;
+    }
+
+    public String getCpf() {
+	return cpf;
+    }
+
+    public Date getNascimento() {
+	return nascimento;
+    }
+
+    public List<String> getTelefone() {
+	return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+	this.telefone.add(telefone);
+    }
+
+    public SexoEnum getSexo() {
+	return sexo;
+    }
+
+    public Endereco getEndereco() {
+	return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+	this.endereco = endereco;
+    }
+
+    public PerfilEnum getPerfil() {
+	return perfil;
+    }
+
+    /*public void setPerfil(PerfilEnum perfil) {
+	this.perfil = perfil;
+    }*/
+
+    public List<ContatoDeEmergencia> getContatosDeEmergencia() {
+	return contatosDeEmergencia;
+    }
+
+    public void addContatoDeEmergencia(ContatoDeEmergencia contatoDeEmergencia) {
+	contatosDeEmergencia.add(contatoDeEmergencia);
+    }
+
 }
