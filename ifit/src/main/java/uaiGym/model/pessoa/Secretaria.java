e74c141e8537ccf1ab4a5360c9c181b6c7489c68
Gledson Junior <groliveirajr@gmail.com>
2020-03-08 16:15:41 -0300

    Add private aos atributos e substituido as List por Set
    Add private aos atributos e substituido as List por Set para nao cadastrar o mesmo obj

--- ifit/src/main/java/uaiGym/model/pessoa/Secretaria.java
+++ ifit/src/main/java/uaiGym/model/pessoa/Secretaria.java
@@ -1,63 +1,64 @@
 package uaiGym.model.pessoa;
 
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 import uaiGym.model.Endereco;
+import uaiGym.model.Equipamento;
 import uaiGym.model.enuns.PerfilEnum;
 import uaiGym.model.enuns.SexoEnum;
 
 public class Secretaria extends Funcionario {
-    List<Aluno> alunos;
-    List<Equipamento> equipamentos;
-    Map<Aluno, Instrutor> ligacaoAlunoInstrutor;
+	
+	private Set<Aluno> alunos;
+	private Set<Equipamento> equipamentos;
+	private Map<Aluno, Instrutor> ligacaoAlunoInstrutor;
 
-    public Secretaria(String nome, String cpf, Date nascimento, String telefone, SexoEnum sexo, Endereco endereco,
-	    int codigoFuncional, int numeroContrato, Calendar admissao, PerfilEnum perfil) {
-	super(nome, cpf, nascimento, telefone, sexo, endereco, codigoFuncional, numeroContrato, admissao, perfil);
-	alunos = new ArrayList<>();
-	equipamentos = new ArrayList<>();
-	ligacaoAlunoInstrutor = new HashMap<>();
-    }
+	public Secretaria(String nome, String cpf, Date nascimento, String telefone, SexoEnum sexo, Endereco endereco,
+			int codigoFuncional, int numeroContrato, Calendar admissao, PerfilEnum perfil) {
+		super(nome, cpf, nascimento, telefone, sexo, endereco, codigoFuncional, numeroContrato, admissao, perfil);
+		alunos = new HashSet<>();
+		equipamentos = new HashSet<>();
+		ligacaoAlunoInstrutor = new HashMap<>();
+	}
 
-    public void cadastroAlunos(Aluno aluno) {
-	if (aluno != null)
-	    alunos.add(aluno);
-    }
+	public void cadastroAlunos(Aluno aluno) {
+		if (aluno != null)
+			alunos.add(aluno);
+	}
 
-    public void cadastroEquipamento(Equipamento equipamento) {
-	if (equipamento != null)
-	    equipamentos.add(equipamento);
-    }
+	public void cadastroEquipamento(Equipamento equipamento) {
+		if (equipamento != null)
+			equipamentos.add(equipamento);
+	}
 
-    public void criarLigacaoAlunoProfessor(Aluno aluno, Instrutor instrutor) {
-	if (aluno != null && instrutor != null)
-	    this.ligacaoAlunoInstrutor.put(aluno, instrutor);
-    }
+	public void criarLigacaoAlunoProfessor(Aluno aluno, Instrutor instrutor) {
+		if (aluno != null && instrutor != null)
+			this.ligacaoAlunoInstrutor.put(aluno, instrutor);
+	}
 
-    public List<Aluno> getAlunos() {
-	return alunos;
-    }
+	public Set<Aluno> getAlunos() {
+		return alunos;
+	}
 
-    public List<Equipamento> getEquipamentos() {
-	return equipamentos;
-    }
+	public Set<Equipamento> getEquipamentos() {
+		return equipamentos;
+	}
 
-    public Instrutor getInstrutorPorAluno(Aluno aluno) {
-	return this.ligacaoAlunoInstrutor.get(aluno);
-    }
+	public Instrutor getInstrutorPorAluno(Aluno aluno) {
+		return this.ligacaoAlunoInstrutor.get(aluno);
+	}
 
-    public Set<Aluno> getAlunorPorInstrutor(Instrutor instrutor) {
-	return instrutor.getAlunos();
-    }
+	public Set<Aluno> getAlunorPorInstrutor(Instrutor instrutor) {
+		return instrutor.getAlunos();
+	}
 
-    public void remocaoAluno(Aluno aluno) {
-	if(this.alunos.contains(aluno)) 
-	    this.alunos.remove(aluno);
-    }
+	public void remocaoAluno(Aluno aluno) {
+		if (this.alunos.contains(aluno))
+			this.alunos.remove(aluno);
+	}
 }
