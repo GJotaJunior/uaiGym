75ce0567ac6ec78e8db409b83ea6f60d20c8ebb9
Wanderson Gutemberg <wanderson.gutembergs@gmail.com>
2020-04-07 23:56:30 -0300

    corrigindo cagada do gledson

--- ifit/src/main/resources/Database/sp_atualiza_usuario.sql
+++ ifit/src/main/resources/Database/sp_atualiza_usuario.sql
@@ -4,7 +4,7 @@ CREATE PROCEDURE `sp_atualiza_usuario` (
 	IN _idUsuario INT, _perfil ENUM('ALUNO', 'INSTRUTOR', 'RECEPCAO', 'GERENTE'), _nome VARCHAR(255),
     _cpf CHAR(11), _dtNascimento DATE, _sexo ENUM('M', 'F'), _email VARCHAR(255), _senha VARCHAR(255),
     _logradouro VARCHAR(255), _numero CHAR(6), _complemento VARCHAR(100),
-    _bairro VARCHAR(50), _cidade VARCHAR(80), _uf CHAR(2), _cecriacaop CHAR(8))
+    _bairro VARCHAR(50), _cidade VARCHAR(80), _uf CHAR(2), _cep CHAR(8))
     
 BEGIN
 	UPDATE Usuario
