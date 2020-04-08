975a4ec4921c7b2ec25f3e5f2280cb0d2d25b474
Nawan <nawandx743@gmail.com>
2020-04-07 22:21:50 -0300

    Atualização procedure aluno e funcionário

--- ifit/src/main/resources/Database/sp_inserirFuncionario.sql
+++ ifit/src/main/resources/Database/sp_inserirFuncionario.sql
@@ -4,13 +4,11 @@ CREATE PROCEDURE `sp_inserirFuncionario`(IN _idUsuario INT, _perfil ENUM('ALUNO'
 _senha VARCHAR(255), _contrato VARCHAR(255), _dtAdmissao DATE, _logradouro VARCHAR(255), _numero CHAR(6), _complemento VARCHAR(100), _bairro VARCHAR(50), _cidade VARCHAR(80), _uf CHAR(2), _cep CHAR(8))
 
 BEGIN
-	DECLARE _idUsuario INT;
 	
 	CALL sp_inserirUsuario(_perfil, _nome, _cpf, _dtNascimento, _sexo, _email, _senha, _logradouro, _numero, _complemento, _bairro, _cidade, _uf, _cep, @_idUsuario);
 	
-	_idUsuario = @_idUsuario;
 	
-	INSERT INTO Funcionario(idUsuario, contrato, dtAdmissao) VALUES (_idUsuario, _contrato, _dtAdmissao);
+	INSERT INTO Funcionario(idUsuario, contrato, dtAdmissao) VALUES (@_idUsuario, _contrato, _dtAdmissao);
 
 END $$
 DELIMITER ;
