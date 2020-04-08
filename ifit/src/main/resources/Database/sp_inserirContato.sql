595783addbba94c82009fdfca0f56e0b613a37eb
Nawan <nawandx743@gmail.com>
2020-04-07 22:50:55 -0300

    Correção tabelas

--- ifit/src/main/resources/Database/sp_inserirContato.sql
+++ ifit/src/main/resources/Database/sp_inserirContato.sql
@@ -1,10 +1,10 @@
 DELIMITER $$
 
-CREATE PROCEDURE `sp_inserirContato`(_idAluno INT, _nome VARCHAR(255) NOT NULL, _telefone VARCHAR(20), _parentesco VARCHAR(15))
+CREATE PROCEDURE `sp_inserirContato`(_idAluno INT, _nome VARCHAR(255) NOT NULL, _telefone VARCHAR(15), _parentesco VARCHAR(15))
 
 BEGIN
 
-	INSERT INTO Endereco(idAluno, nome, telefone, parentesco) VALUES (_idAluno, _nome, _telefone, _parentesco);
+	INSERT INTO Contato(idAluno, nome, telefone, parentesco) VALUES (_idAluno, _nome, _telefone, _parentesco);
 
 END $$
 DELIMITER ;
--- ifit/src/main/resources/Database/sp_inserirTelefone.sql
+++ ifit/src/main/resources/Database/sp_inserirTelefone.sql
@@ -4,7 +4,7 @@ CREATE PROCEDURE `sp_inserirContato`(_idUsuario INT, _telefone VARCHAR(15))
 
 BEGIN
 
-	INSERT INTO Endereco(idUsuario, telefone) VALUES (_idUsuario, _telefone);
+	INSERT INTO Telefone(idUsuario, telefone) VALUES (_idUsuario, _telefone);
 
 END $$
 DELIMITER ;
