98a83eb486897e6be29f0f91e1a158d1d4f22102
Gledson Junior <groliveirajr@gmail.com>
2020-04-14 23:52:33 -0300

    alterado algoritmo para criptografar parametro
    alterado algoritmo para criptografar parametro

--- ifit/src/main/java/uaiGym/service/EncryptionService.java
+++ ifit/src/main/java/uaiGym/service/EncryptionService.java
@@ -7,18 +7,24 @@ import java.nio.charset.StandardCharsets;
 
 public class EncryptionService {
 
+	// TODO implementar um algoritmo de criptografar
+
+	private static final String AFTER_ID = "15878645431";
+	private static final String BEFORE_ID = "983456873132";
+
 	public static String linkGenerator(Integer id) throws UnsupportedEncodingException {
 		String url = "/redefinir-senha?id=" + encrypt(id.toString());
 
 		return url;
 	}
 
-	private static String encrypt(String argument) throws UnsupportedEncodingException {
-		return URLEncoder.encode(argument, StandardCharsets.UTF_8.toString());
+	public static String encrypt(String argument) throws UnsupportedEncodingException {
+		return AFTER_ID + URLEncoder.encode(argument.toString(), StandardCharsets.UTF_8.toString()) + BEFORE_ID;
 	}
-	
+
 	public static String decrypt(String argument) throws UnsupportedEncodingException {
-		return URLDecoder.decode(argument, StandardCharsets.UTF_8.toString());
+		return URLDecoder.decode(argument.toString(), StandardCharsets.UTF_8.toString()).replace(AFTER_ID, "")
+				.replace(BEFORE_ID, "");
 	}
 
 }
