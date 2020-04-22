4ef92a2171eec0de72e2776ed1eb063d9779b62d
Gledson Junior <groliveirajr@gmail.com>
2020-04-22 19:54:41 -0300

    Criado construtor com todos os atributos

--- ifit/src/main/java/uaiGym/model/pessoa/Gerente.java
+++ ifit/src/main/java/uaiGym/model/pessoa/Gerente.java
@@ -1,20 +1,38 @@
 package uaiGym.model.pessoa;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Set;
+
+import uaiGym.model.Endereco;
+import uaiGym.model.enuns.PerfilEnum;
+import uaiGym.model.enuns.SexoEnum;
 
 public class Gerente extends Funcionario {
-    List<Instrutor> instrutores;
-    List<Recepcao> secretariado;
-    public List<Instrutor> getInstrutores() {
-        return instrutores;
-    }
-    public void setInstrutores(List<Instrutor> instrutores) {
-        this.instrutores = instrutores;
-    }
-    public List<Recepcao> getSecretariado() {
-        return secretariado;
-    }
-    public void setSecretariado(List<Recepcao> secretariado) {
-        this.secretariado = secretariado;
-    }
+
+	List<Instrutor> instrutores;
+	List<Recepcao> secretariado;
+	
+	public Gerente(String email, String senha, String nome, String cpf, Date nascimento, Set<String> telefones,
+			SexoEnum sexo, Endereco endereco, PerfilEnum perfil, String contrato, Date admissao, Date demissao) {
+		super(email, senha, nome, cpf, nascimento, telefones, sexo, endereco, perfil, contrato, demissao, demissao);
+		instrutores = new ArrayList<Instrutor>();
+	}
+
+	public List<Instrutor> getInstrutores() {
+		return instrutores;
+	}
+
+	public void setInstrutores(List<Instrutor> instrutores) {
+		this.instrutores = instrutores;
+	}
+
+	public List<Recepcao> getSecretariado() {
+		return secretariado;
+	}
+
+	public void setSecretariado(List<Recepcao> secretariado) {
+		this.secretariado = secretariado;
+	}
 }
