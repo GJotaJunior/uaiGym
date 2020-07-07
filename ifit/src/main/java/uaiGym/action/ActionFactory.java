2845c393e9e236b7ab80f54527b75857c8c3c493
Gledson Junior <49466350+GJotaJunior@users.noreply.github.com>
2020-07-07 16:23:12 -0300

    listagem de avalicaoes do aluno

--- ifit/src/main/java/uaiGym/action/ActionFactory.java
+++ ifit/src/main/java/uaiGym/action/ActionFactory.java
@@ -7,34 +7,36 @@ import javax.servlet.http.HttpServletRequest;
 
 public class ActionFactory {
 
-    static Map<String, Action> actions = new HashMap<String, Action>();
-    static {
-	actions.put("GET/", new LoginAction());
-	actions.put("GET/cadastrar", new RegisterAction());
-	actions.put("GET/sair", new LogoutAction());
-	actions.put("GET/redefinir-senha", new ResetPasswordAction());
+	static Map<String, Action> actions = new HashMap<String, Action>();
+	static {
+		actions.put("GET/", new LoginAction());
+		actions.put("GET/cadastrar", new RegisterAction());
+		actions.put("GET/sair", new LogoutAction());
+		actions.put("GET/redefinir-senha", new ResetPasswordAction());
 
-	actions.put("POST/", new LoginAction());
-	actions.put("POST/cadastrar", new RegisterAction());
-	actions.put("POST/redefinir-senha", new ResetPasswordAction());
-	actions.put("POST/redefinir-senha-confirma", new ResetPasswordConfirmAction());
+		actions.put("POST/", new LoginAction());
+		actions.put("POST/cadastrar", new RegisterAction());
+		actions.put("POST/redefinir-senha", new ResetPasswordAction());
+		actions.put("POST/redefinir-senha-confirma", new ResetPasswordConfirmAction());
 
-	actions.put("GET/instrutor/", new InstrutorListAction());
-	actions.put("GET/instrutor/listagem", new InstrutorListAction());
-	actions.put("GET/instrutor/cadastrar", new InstrutorRegisterAction());
-	actions.put("POST/instrutor/cadastrar", new InstrutorRegisterAction());
-	
-	actions.put("GET/aluno/", new AlunoListAction());
-	actions.put("GET/aluno/listagem", new AlunoListAction());
-	actions.put("GET/aluno/cadastrar", new AlunoRegisterAction());
-	actions.put("POST/aluno/cadastrar", new AlunoRegisterAction());
-    }
+		actions.put("GET/instrutor", new InstrutorListAction());
+		actions.put("GET/instrutor/listagem", new InstrutorListAction());
+		actions.put("GET/instrutor/cadastrar", new InstrutorRegisterAction());
+		actions.put("POST/instrutor/cadastrar", new InstrutorRegisterAction());
 
-    public static Action getAction(HttpServletRequest request) {
-	String method = request.getMethod();
-	String url = request.getPathInfo().toLowerCase();
+		actions.put("GET/aluno", new AlunoListAction());
+		actions.put("GET/aluno/listagem", new AlunoListAction());
+		actions.put("GET/aluno/cadastrar", new AlunoRegisterAction());
+		actions.put("POST/aluno/cadastrar", new AlunoRegisterAction());
+		
+		actions.put("GET/avaliacoes", new AvaliacoesListAction());
+	}
 
-	Action action = actions.getOrDefault(method + url, new NULLAction());
-	return action;
-    }
+	public static Action getAction(HttpServletRequest request) {
+		String method = request.getMethod();
+		String url = request.getPathInfo().toLowerCase();
+
+		Action action = actions.getOrDefault(method + url, new NULLAction());
+		return action;
+	}
 }
